2d16e5924bea6cbd866c0141e61ebd32373214ba
patilrutuja436 <68347539+patilrutuja436@users.noreply.github.com>
2020-07-17 17:35:35 +0530

    Add files via upload

---
+++ Assignment2/UserInput.js
@@ -0,0 +1,3 @@
+//Write a js code which takes input from user and logs it in the console.
+let age=prompt("Enter Your age");
+console.log(`Age= ${age}`);
---
+++ Assignment2/ques3.js
@@ -0,0 +1,11 @@
+//Ask the user if he is 21+ and log the value "Can drink","Cannot drink".
+let age=prompt("Enter Your age");
+if(age>21)
+{
+	console.log("Can Drink");
+	
+}
+else{
+	console.log("Cannot Drink");
+}
+
